852e79a56d24213915418129bb0863516c678a53
Pibodee <mrbodyanuch@gmail.com>
2023-01-15 20:47:42 +0200

    Beta 1.0
    Deleted console logs

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ function onInput() {
 
   Countries.fetchCountries(name)
     .then(data => {
-      console.log(data);
       if (data.length > 10) {
         reset();
         Notiflix.Notify.info(
