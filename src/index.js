2a8d8c71cf89935abd7d1fe9588472a954be2b19
Pibodee <mrbodyanuch@gmail.com>
2023-01-15 16:44:39 +0200

    fixed list

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
+const box = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
@@ -19,21 +20,42 @@ function onInput() {
     if (data.length > 10) {
       Notiflix.Notify.info(
         'Too many matches found. Please enter a more specific name.'
-      );
-    } else if (data.length <= 10 && data.length >= 2) {
+        )
+      };
+    if (data.length <= 10 && data.length >= 2) {
       createList(data);
-    }
+      };
+      if (data.length === 1) {
+        createBox(data[0])
+    }      
   });
 
   console.log(`${BASE_URL}${name}`);
 }
 
-function createList(countryList) {
-  list.innerHTML = countryList
+function createList(arr) {
+    clearFields();
+  list.innerHTML = arr
     .map(country => 
-      `<li>
-      <img src = "${country.flags.svg}" width = "24px" alt = "${country.name}">
+      `<li class = "country-item">
+      <img src = "${country.flags.svg}" width = "40px" alt = "${country.name}">
       <h3>${country.name.common}</h3>
     </li>`)
     .join('');
+}
+
+function createBox(obj) {
+    clearFields();
+    box.innerHTML = `<img src="${obj.flags.svg}" alt="${obj.name}">
+    <h3>${obj.name}</h3>
+    <ul>
+      <li><span>Capital: </span>${obj.capital}</li>
+      <li><span>Population: </span>${obj.population}</li>
+      <li><span>Languages: </span>${obj.languages}</li>
+    </ul>`;
+}
+
+function clearFields() {
+    list.innerHTML = '';
+    box.innerHTML = '';
 }
